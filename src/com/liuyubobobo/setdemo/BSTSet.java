8338062a7e3369d43113ce8e2462693a6629fb81
chenjian <chenjian_336@126.com>
2019-11-17 16:15:12 +0800

    apple study commit

---
+++ src/com/liuyubobobo/setdemo/BSTSet.java
@@ -0,0 +1,30 @@
+package com.liuyubobobo.setdemo;
+
+public class BSTSet<E extends Comparable<E>> implements Set<E> {
+
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
---
+++ src/com/liuyubobobo/setdemo/Set.java
@@ -0,0 +1,14 @@
+package com.liuyubobobo.setdemo;
+
+public interface Set<E> {
+
+    void add(E e);
+
+    boolean contains(E e);
+
+    void remove(E e);
+
+    int getSize();
+
+    boolean isEmpty();
+}
